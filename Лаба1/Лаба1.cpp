93729d4c32bfd502834089d43824be4f2e29bf83
Мария Мелентьева <97579059+Wersu@users.noreply.github.com>
2022-03-25 18:29:40 +0500

    Добавьте файлы проекта.

---
+++ Лаба1/Лаба1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <GL/freeglut.h>
+
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+
+    glutDisplayFunc(RenderSceneCB);
+
+    glClearColor(0.4f, 0.0f, 0.5f, 0.0f);
+
+    glutMainLoop();
+
+    return 0;
+}
