ba1d43a3f3ddccbc6a1be5ccde167db6e04af7e4
Мария Мелентьева <97579059+Wersu@users.noreply.github.com>
2022-03-25 20:20:44 +0500

    Tutorial 3

--- Лаба1/Лаба1.cpp
+++ Лаба1/Лаба1.cpp
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include "math_3d.h"
+#include <glm\vec3.hpp>s
 
 GLuint VBO;
 
@@ -13,29 +13,12 @@ static void RenderSceneCB()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDrawArrays(GL_POINTS, 0, 1);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glDisableVertexAttribArray(0);
 
     glutSwapBuffers();
 }
-
-
-static void InitializeGlutCallbacks()
-{
-    glutDisplayFunc(RenderSceneCB);
-}
-
-static void CreateVertexBuffer()
-{
-    Vector3f Vertices[1];
-    Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
-
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-}
-
 
 int main(int argc, char** argv)
 {
@@ -45,7 +28,7 @@ int main(int argc, char** argv)
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 02");
 
-    InitializeGlutCallbacks();
+    glutDisplayFunc(RenderSceneCB);
 
     GLenum res = glewInit();
     if (res != GLEW_OK) {
@@ -55,7 +38,15 @@ int main(int argc, char** argv)
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    CreateVertexBuffer();
+    glm::vec3 Vertices[3];
+    Vertices[0] = glm::vec3(0.0f, 0.33f, 0.0f);
+    Vertices[1] = glm::vec3(0.33f, 1.0f, 0.0f);
+    Vertices[2] = glm::vec3(-0.33f, 1.0f, 0.0f);
+
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
     glutMainLoop();
 
