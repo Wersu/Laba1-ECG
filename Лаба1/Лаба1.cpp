0c311c499918e3531cc3a0f44682c5dec9574826
Мария Мелентьева <97579059+Wersu@users.noreply.github.com>
2022-03-26 14:41:49 +0500

    Tutorial 2

--- Лаба1/Лаба1.cpp
+++ Лаба1/Лаба1.cpp
@@ -13,7 +13,7 @@ static void RenderSceneCB()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_POINTS, 0, 1);
 
     glDisableVertexAttribArray(0);
 
@@ -38,10 +38,9 @@ int main(int argc, char** argv)
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    glm::vec3 Vertices[3];
-    Vertices[0] = glm::vec3(0.0f, 0.33f, 0.0f);
-    Vertices[1] = glm::vec3(0.33f, 1.0f, 0.0f);
-    Vertices[2] = glm::vec3(-0.33f, 1.0f, 0.0f);
+    glm::vec3 Vertices[1];
+    Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+
 
 
     glGenBuffers(1, &VBO);
